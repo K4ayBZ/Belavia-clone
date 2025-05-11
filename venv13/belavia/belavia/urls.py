b709a8e5e701f9f5b8411158878b24cf4e590a6e
K4ayBZ <1danilademidov@gmail.com>
2025-05-11 11:27:34 +0300

    comment

--- venv13/belavia/belavia/urls.py
+++ venv13/belavia/belavia/urls.py
@@ -45,10 +45,6 @@ urlpatterns = [
     path('flights/', include('flights.urls')),
 ]
 
-from django.contrib import admin
-from django.urls import path, include
-from django.contrib.auth import views as auth_views
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
